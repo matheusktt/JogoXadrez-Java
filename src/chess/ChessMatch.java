27feb00a13cb9de1952b0e987f0d9fd5b0c06443
Matheus Oliveira <matheus_0119@outlook.com>
2022-03-08 13:54:50 -0300

    Adicionando posições e Exceções

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -3,7 +3,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -31,10 +30,14 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePice(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 //	Inicia as peças na posição padrão da partida
 	private void initialSetup() {
-		board.placePice(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePice(new King(board, Color.WHITE), new Position(7, 4));
-		board.placePice(new King(board, Color.BLACK), new Position(0, 4));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 }
