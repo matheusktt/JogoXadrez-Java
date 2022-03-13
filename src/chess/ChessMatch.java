15404822c6ec2a982a833a02d68705da0a6b7cc1
Matheus Oliveira <matheus_0119@outlook.com>
2022-03-13 12:02:41 -0300

    Imprimindo movimentos no tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,6 +29,14 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+	
+//	Posições possiveis a partir da origem
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+
 
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 //		posições da matriz
