4cfe84c01aea9541079efe7a503d19e83339da18
Matheus Oliveira <matheus_0119@outlook.com>
2022-03-13 13:00:55 -0300

    Lógica de check

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,6 +17,10 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
 
 //	Função será reaproveitada pelo pacote e subclasses
 //	Verifica a cor da peça é diferente da peça selecionada
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -31,7 +31,7 @@ public class ChessPosition {
 	
 //	converte para posição de xadrez
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
